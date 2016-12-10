0b538e44a1ef3409586b045cc8b843574857d214
Stephen Whitmore <sww@eight45.net>
2016-12-10 09:36:12 -0500

    Remove unused test.

--- test/formset.js
+++ test/formset.js
@@ -77,27 +77,3 @@ test('full proper form set (attach, form, attach)', function (t) {
 
   t.end()
 })
-
-// TODO(sww): add this to index.js, not FormSet
-// test('attachment matching', function (t) {
-//   var set = new FormSet()
-
-//   var form = {
-//     id: 'foo',
-//     things: [
-//       'short.mov',
-//       'long.3gpp',
-//       'bob.',
-//       '.avi',
-//       'I have spaces.New sentence',
-//       'this-maybe_will?match.17pm'
-//     ]
-//   }
-//   set.addForm(form, form.things)
-
-//   t.deepEqual(set.getMissingAttachmentNames(), ['short.mov', 'long.3gpp', 'this-maybe_will?match.17pm'])
-//   t.equal(set.getOrphanAttachmentNames().length, 0)
-//   t.equal(set.forms.length, 1)
-
-//   t.end()
-// })
